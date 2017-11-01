0d986ddb30d305f8755477ed93c47938dfb97437
Anant Jain <anantjain60@gmail.com>
2017-11-01 23:13:14 +0530

    Update and rename dashboard.html to dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,39 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Dashboard</title>
-  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Acme|Josefin+Sans|Josefin+Slab|Libre+Baskerville|Lobster|Lobster+Two|Montserrat|Nunito|Playfair+Display+SC|Poiret+One|Questrial|Quicksand|Roboto|Sanchez|Varela+Round" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora|Quicksand|Roboto|Roboto+Slab" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
-    <link rel="stylesheet" type="text/css" href="dashboard.css">
-</head>
-<body>
-<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
-        <div class="container-fluid topnav">
-            
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="index.html" class="navbar-brand" title="Homepage"><img src="img/logo.png"></a>
-            </div>
-            
-            <div class="collapse navbar-collapse navbar-right" id="mainNavbar">
-                <ul class="nav navbar-nav" >
-                  <li id="link"><a href="index.html" id="link"><i class="glyphicon glyphicon-home"></i></a></li>
-                </ul>
-            </div>
-        </div>
-</nav>
+<?php
+session_start();
+
+if(isset($_SESSION['myusername']))
+{
+    include 'config.php';
+    include 'head.php';
+
+?>
+
 <div class="container-fluid">
   <div class="row">
 
@@ -197,3 +171,14 @@
 </div>
 </body>
 </html>
+
+<?php
+	mysql_close($connection);
+}
+else
+{
+     echo"<script type='text/javascript'>
+     window.location.href='login.php';
+     </script>";
+}
+?>
